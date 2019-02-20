081b41814190fc928c2b9ea7baedbe6de263b35c
Janelle de Ment <janelledement@gmail.com>
2019-02-20 09:36:51 -0400

    Does not call the zero-watch listener when deregistered first

--- src/scope.js
+++ src/scope.js
@@ -180,10 +180,15 @@ Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
   var firstRun = true;
 
   if (watchFns.length === 0) {
+    var shouldCall = true;
     self.$evalAsync(function () {
-      listenerFn(newValues, newValues, self);
+      if (shouldCall) {
+        listenerFn(newValues, newValues, self);
+      }
     });
-    return;
+    return function () {
+      shouldCall = false;
+    };
   }
 
   function watchGroupListerner() {
@@ -211,7 +216,7 @@ Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
     _.forEach(destroyFunctions, function(destroyFunction) {
       destroyFunction();
     });
-  }
+  };
 };
 
 module.exports = Scope;
