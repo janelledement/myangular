3d7203fed775cd2de4f133fd0fa12be857a8a57f
Janelle de Ment <janelledement@gmail.com>
2019-02-26 10:50:42 -0400

    Each child gets its own watches array

--- src/scope.js
+++ src/scope.js
@@ -223,6 +223,7 @@ Scope.prototype.$new = function () {
   var ChildScope = function () {};
   ChildScope.prototype = this;
   var child = new ChildScope();
+  child.$$watchers = [];
   return child;
 };
 
